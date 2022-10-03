cc9072d89c6e05b8761d40036f360db905462e68
manumasjoan <manumasjoan@gmail.com>
2022-10-03 14:29:49 -0300

    fix shape detection

--- detector.py
+++ detector.py
@@ -1,6 +1,9 @@
+from operator import invert
+from turtle import distance
 import cv2 as cv
 import numpy as np
 import sys
+import math
 
 
 def read_camera_parameters(filepath = 'camera_parameters/intrinsic.dat'):
@@ -49,7 +52,8 @@ def get_qr_coords(cmtx, dist, points):
 def show_axes(cmtx, dist, img):
     qr = cv.QRCodeDetector()
 
-    ret_qr, points = qr.detect(img)
+    #ret_qr, points = qr.detect(img)
+    ret_qr, points = qr.detect(cv.bitwise_not(img))
 
     if ret_qr:
         #axis_points devuelve las coordenadas de las 4 puntas del QR
@@ -66,6 +70,10 @@ def show_axes(cmtx, dist, img):
             axis_points = axis_points.reshape((4,2))
 
             origin = (int(axis_points[0][0]),int(axis_points[0][1]) )
+            #cv.line(img, origin, (int(axis_points[1][0]),int(axis_points[1][1]) ), (255,255,0), 5)
+            #print("distance1"+ str(distance))
+            #print("distance2"+ str(distance2))
+
             show_centers(img,origin)
 
             # for p, c in zip(axis_points[1:], colors[:3]):
@@ -77,37 +85,84 @@ def show_axes(cmtx, dist, img):
             #     if p[0] > 5*img.shape[1] or p[1] > 5*img.shape[1]:break
 
             #     cv.line(img, origin, p, c, 5)
+def get_binary_image(image, value):
+    gray_image = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
+    ret, threshold = cv.threshold(gray_image, value, 255, cv.THRESH_BINARY_INV)
+    return threshold
+    
+def get_denoised_image(binary):
+    structuring_element = cv.getStructuringElement(cv.MORPH_ELLIPSE, (5, 5))
+    morph_open = cv.morphologyEx(binary, cv.MORPH_OPEN, structuring_element)
+    morph_close = cv.morphologyEx(binary, cv.MORPH_CLOSE, structuring_element)
+    return morph_close
+
+def get_biggest_contour(contours):
+    max_cnt = contours[1]
+    for cnt in contours:
+        # print(cnt)
+        if cv.contourArea(cnt) > cv.contourArea(max_cnt):
+            max_cnt = cnt
+    return max_cnt
 
 def show_centers(img, origin):
 
-    #Aplico threshold a la imagen
-    gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
-    _,th= cv.threshold(gray,150,255, cv.THRESH_BINARY)
+    # #Aplico threshold a la imagen
+    # gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+    # _,th= cv.threshold(gray,150,255, cv.THRESH_BINARY)
 
-    #Aplico operaciones morfologicas
-    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (4,4))
-    opening = cv.morphologyEx(th, cv.MORPH_OPEN, kernel)
-    closing = cv.morphologyEx(opening, cv.MORPH_CLOSE, kernel)
+    # #Aplico operaciones morfologicas
+    # kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (4,4))
+    # opening = cv.morphologyEx(th, cv.MORPH_OPEN, kernel)
+    # closing = cv.morphologyEx(opening, cv.MORPH_CLOSE, kernel)
+
+     #original image
+    #ret, original_image = webcam.read()
+    # cv.imshow('Original', original_image)
+
+    #binary image
+    # binary_value = cv.getTrackbarPos('Binary_Trackbar', 'Binary')
+    binary_image = get_binary_image(img, 50)
+    cv.imshow('Binary', binary_image)
+
+    #denoised image
+    denoised_image = get_denoised_image(binary_image)
+    cv.imshow('Denoised', denoised_image)
+
+    #contours
+    _, contours, _ = cv.findContours(denoised_image, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
 
     #Busco los contornos
-    _, contornos, _ = cv.findContours(closing, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+   # _, contornos, _ = cv.findContours(closing, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
     #filtro los contornos
     contornos_filtrados = [] #inicializo la variable de contornos filtrados
-    for c in contornos:
+    for c in contours:
         area = cv.contourArea(c)
         if area > 500 and area < 2000:
+            
             #en un vector voy guardando los contornos, el append lo que hace es ir agregando el ultimo contorno y lo escribe
             contornos_filtrados.append(c)
+            pto = c
+            M = cv.moments(pto)
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+            if(math.dist(origin,[cX,cY])>200):
+                cv.drawContours(image=img, contours=[c], contourIdx=-1, color=(0, 255, 0), thickness=3)
+                cv.line(img, origin, [cX,cY], (0, 255, 0), 5)
 
    
     #ejemplo con un solo punto 
-    if(len(contornos_filtrados)>0):
-        pto = contornos_filtrados[0]
-        M = cv.moments(pto)
-        cX = int(M["m10"] / M["m00"])
-        cY = int(M["m01"] / M["m00"])
-        cv.line(img, origin, [cX,cY], (0, 255, 0), 5)
+    # if(len(contornos_filtrados)>0):
+    #     pto = contornos_filtrados[0]
+    #     M = cv.moments(pto)
+    #     cX = int(M["m10"] / M["m00"])
+    #     cY = int(M["m01"] / M["m00"])
+    #     cv.line(img, origin, [cX,cY], (0, 255, 0), 5)
+    #     print("------")
+    #     print("distancia en x:"+ str(cX-origin[0]))
+    #     print("distancia en Y:"+ str(cY-origin[1]))
+    #     print("------")
+
         #cv.circle(img, (cX, cY), 1, (0, 255, 0), 5)
 
 
@@ -122,6 +177,7 @@ def show_centers(img, origin):
     #         Coordenadas_X.append(cX)
     #         Coordenadas_Y.append(cY)
 
+        cv.imshow('Binary', cv.bitwise_not(img))
 
 
 def execute(cmtx, dist, in_source):
---
+++ robot_commands.py
@@ -0,0 +1,145 @@
+from urx import robotiq_two_finger_gripper
+import time
+import socket
+from typing import List
+import urx
+import cv2 as cv
+
+HOST = "192.168.0.16" # IP del robot
+PORT = 30002 # port: 30001, 30002 o 30003, en ambos extremos
+
+rob = urx.Robot(HOST)
+robotiqgrip = robotiq_two_finger_gripper.Robotiq_Two_Finger_Gripper(rob)
+print("conectando a gripper...")
+time.sleep(1)
+
+
+print("Conectando a IP: ", HOST)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("conectando...")
+s.connect((HOST, PORT))
+time.sleep(0.5)
+print("Conectado con el robot")
+
+
+
+# # Conexion con la camara
+# print("Conectando con la camara")
+# cap = cv.VideoCapture(0, cv.CAP_DSHOW)
+# print(cap.isOpened())
+# tree = trainingModel.train_model()
+# cv.namedWindow('Parameters')
+# print("Conectado con la camara")
+
+
+# # posiciones cargadas
+# posiciones = {  "0" : b"movej([-3.2189834753619593, -1.7424808941283167, 1.4784472624408167, -1.4887936872294922, -1.6161873976336878, -0.18661243120302373])\n",
+#                 "2" : b"movej([-3.956921402608053, -1.7561424236693324, 2.4691947142230433, -2.2455297909178675, -1.5485871473895472, -0.8221257368670862])\n", 
+#                 "1" : b"movej([-3.5026300589190880, -1.0280173772624512, 1.5461886564837855, -2.1145335636534632, -1.6231797377215784, -0.32508117357362920])\n",
+#                 "3" : b"movej([-4.226582352315084, -1.4589700521281739, 2.188515011464254, -2.300427099267477, -1.5482633749591272, -1.1085799376117151])\n",
+#                 "4" : b"movej([-3.790581528340475, -0.9107036413005372, 1.3540595213519495, -2.0169321499266566, -1.540293041859762, -0.6730135122882288])\n",
+#                 "5" : b"movej([-3.8255489508258265, -1.4982145589641114, 1.3549464384662073, -1.4924248021892090, -1.6503947416888636, -0.75999957719911750])\n" }
+
+
+# coordenadas_camara = {  "3" : (350, 310),
+#                         "4" : (15 , 295),
+#                         "2" : (365, 135),
+#                         "1" : (30 , 110)}
+
+
+# movimiento de robot
+
+def move(p) :
+    s.send( posiciones[p])
+    time.sleep(2)
+
+
+# deteccion de posiciones ocupadas
+def detect_objects (): 
+
+    timer = 0
+
+    while timer < 2:
+        _, frame = cap.read()
+        frame = cv.flip(frame, 1)
+        area_thresh = 300
+        binary_thresh = 100
+        color_img , binary_img, coords = testingModel.load_and_test(tree, frame, area_thresh, binary_thresh )
+        timer += 1
+        print("Se encontraron objetos en las siguientes coordenadas: " + str(coords))
+
+    return coords
+
+# verifies if position is occupied
+
+def is_position_occupied(p) :
+    object_coordinates = detect_objects()
+    offset = 80
+    time.sleep(4)
+    x, y = coordenadas_camara.get(str(p))
+
+    for c in object_coordinates:
+        if (x < c[0] < x+offset and y < c[1] < y+offset) : return True
+
+    return False
+
+# MAIN
+
+# Inicio del proceso
+
+print("Bienvenido! Inicializando el proceso...")
+rob.send_program(str(robotiqgrip.open_gripper()))
+time.sleep(2)
+move("0")
+time.sleep(2)
+
+
+
+while True:
+
+
+    # ELEGIR POSICION DE PARTIDA
+
+    while True :
+
+       
+        print("Elija de que posicion agarrar un objeto. Ingrese un numero entre [1] y [4]")
+        p_partida = input()
+        if is_position_occupied(p_partida) :
+            move("5")
+            move(p_partida)
+            print("Cerrando gripper")
+            rob.send_program(str(robotiqgrip.close_gripper()))
+            time.sleep(2)
+            move("5")
+            move("0")
+            time.sleep(2)
+            break
+        print("No hay un objeto en esta posicion, elija otra.")
+
+    while True :
+
+        print("Elija en que posicion dejar el objeto. Ingrese un numero entre [1] y [4]")
+        p_destino = input()
+        if not is_position_occupied(p_destino) :
+            move("5")
+            move(p_destino)
+            print("Abriendo gripper")
+            rob.send_program(str(robotiqgrip.open_gripper()))
+            time.sleep(2)
+            move("5")
+            move("0")
+            time.sleep(2)
+            break
+        print("Esta posicion ya esta ocupada, elija otra.")
+
+    print("Ingrese [1] para continuar el proceso, o cualquier otra tecla para finalizarlo:")
+    c = input() 
+    if(c != "1"):
+        break
+    
+
+print("Proceso finalizado")
+
+exit
+
